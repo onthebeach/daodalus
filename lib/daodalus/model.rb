2cc33fcba9ba336922fcb34f509647a243f05090
Russell <russell@russelldunphy.com>
2013-03-27 15:02:23 +0000

    Associations

--- lib/daodalus/model.rb
+++ lib/daodalus/model.rb
@@ -11,43 +11,47 @@ module Daodalus
     end
 
     module DocumentDescriptor
+
+      def has_one(f, options={})
+        define_method f do
+          instance_variable_get(:"@#{f}") ||
+            instance_variable_set(:"@#{f}", options.fetch(:type).new(data.fetch(f.to_s)))
+        end
+      end
+
+      def has_many(f, options={})
+        type = options.fetch(:type)
+        define_method f do
+          instance_variable_get(:"@#{f}") ||
+            instance_variable_set(:"@#{f}", data.fetch(f.to_s).map {|x| type.new(x) } )
+        end
+      end
+
       def field(f, options={})
         key = options.fetch(:key, f)
-        type = options.fetch(:type, nil)
         if options.has_key?(:default)
-          define_field_with_default(f, key, type, options)
+          define_field_with_default(f, key, options)
         else
-          define_field(f, key, type)
+          define_field(f, key)
         end
       end
 
       private
 
-      def define_field(f, key, type)
+      def define_field(f, key)
         define_method f do
-          value = data.fetch(key.to_s)
-          if type.nil? then value else apply_conversion(value, type) end
+          data.fetch(key.to_s)
         end
       end
 
-      def define_field_with_default(f, key, type, options)
+      def define_field_with_default(f, key, options)
         default = options.fetch(:default)
         default_proc = default.is_a?(Proc) ? default : lambda { default }
         define_method f do
-          value = data.fetch(key.to_s, default_proc.call)
-          if type.nil? then value else apply_conversion(value, type) end
+          data.fetch(key.to_s, default_proc.call)
         end
       end
 
-    end
-
-    private
-
-    def apply_conversion(value, type)
-      case type.to_s
-      when 'Symbol' then value.to_sym
-      else type.new(value)
-      end
     end
 
   end
--- spec/lib/daodalus/model_spec.rb
+++ spec/lib/daodalus/model_spec.rb
@@ -14,8 +14,8 @@ class TestModel
   field :y, key: 'the_y_field'
   field :z, default: 5
   field :zz, default: lambda { 5 }
-  field :sym, type: Symbol
-  field :nested, type: NestedTestModel
+  has_one :single_nested, type: NestedTestModel
+  has_many :multiple_nested, type: NestedTestModel
 end
 
 module Daodalus
@@ -28,10 +28,20 @@ module Daodalus
                                  'sym' => 'oeu',
                                  'date' => '20120405',
                                  'time' => '1123',
-                                 'nested' => {
+                                 'single_nested' => {
                                    'foo' => 1,
                                    'bar' => 2
-                                 }) }
+                                 },
+                                 'multiple_nested' => [
+                                   {
+                                     'foo' => 1,
+                                     'bar' => 2
+                                   },
+                                   {
+                                     'foo' => 3,
+                                     'bar' => 4
+                                   }
+                                 ]) }
 
     it 'takes a result' do
       model
@@ -53,14 +63,23 @@ module Daodalus
       model.zz.should eq 5
     end
 
-    it 'can convert fields to symbols' do
-      model.sym.should be_a Symbol
+    it 'can have nested model fields' do
+      model.single_nested.should be_a NestedTestModel
+      model.single_nested.foo.should eq 1
+      model.single_nested.bar.should eq 2
     end
 
-    it 'can have nested model fields' do
-      model.nested.should be_a NestedTestModel
-      model.nested.foo.should eq 1
-      model.nested.bar.should eq 2
+    it 'can have has_many associations' do
+      model.multiple_nested.should be_a Array
+      model.multiple_nested.first.should be_a NestedTestModel
+      model.multiple_nested.last.bar.should eq 4
+    end
+
+    it 'memoizes nested model fields' do
+      NestedTestModel.should_receive(:new).with({"foo"=>1, "bar"=>2}).once.and_return(stub)
+      model.single_nested
+      model.single_nested
+      model.single_nested
     end
   end
 end
