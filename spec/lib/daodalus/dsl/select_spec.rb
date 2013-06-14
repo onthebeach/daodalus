e0c9d9c43dde640e00b974fbf316d32070e3a938
Russell <russell@russelldunphy.com>
2013-06-14 15:43:28 +0100

    Convenience methods to get query, update and projections

--- spec/lib/daodalus/dsl/select_spec.rb
+++ spec/lib/daodalus/dsl/select_spec.rb
@@ -14,6 +14,18 @@ module Daodalus
                                {'type' => 'wet', 'name' => 'whiskas'}])
       end
 
+      it 'allows you to get the current query' do
+        dao.select(:name).to_query.should be_a Hash
+      end
+
+      it 'allows you to get the current projection' do
+        dao.select(:name).to_projection.should be_a Hash
+      end
+
+      it 'allows you to get the current update' do
+        dao.select(:name).to_update.should be_a Hash
+      end
+
       it 'allows the selection of fields to be returned' do
         dao.select(:name, :paws).find_one.value.keys.should eq ['name', 'paws']
       end
--- spec/lib/daodalus/dsl/where_spec.rb
+++ spec/lib/daodalus/dsl/where_spec.rb
@@ -119,6 +119,23 @@ module Daodalus
           dao.where(:type).eq(:dry).and(:name).eq('whiskas')
         ).find_one.should be_none
       end
+
+      it 'can be chained with select clauses' do
+        dao.where(:name).eq('Terry').select(:paws).find_one.value.fetch('paws').should eq 3
+      end
+
+      it 'allows you to get the current query' do
+        dao.where(:name).eq('Trevor').to_query.should be_a Hash
+      end
+
+      it 'allows you to get the current projection' do
+        dao.where(:name).eq('Trevor').to_projection.should be_a Hash
+      end
+
+      it 'allows you to get the current update' do
+        dao.where(:name).eq('Trevor').to_update.should be_a Hash
+      end
+
     end
   end
 end
