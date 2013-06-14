7264d8e4e456ec53ac294a062df54e7b774744af
Russell <russell@russelldunphy.com>
2013-06-14 19:17:11 +0100

    remove unneeded specs

--- spec/lib/daodalus/dsl/where_spec.rb
+++ spec/lib/daodalus/dsl/where_spec.rb
@@ -124,18 +124,6 @@ module Daodalus
         dao.where(:name).eq('Terry').select(:paws).find_one.value.fetch('paws').should eq 3
       end
 
-      it 'allows you to get the current query' do
-        dao.where(:name).eq('Trevor').to_query.should be_a Hash
-      end
-
-      it 'allows you to get the current projection' do
-        dao.where(:name).eq('Trevor').to_projection.should be_a Hash
-      end
-
-      it 'allows you to get the current update' do
-        dao.where(:name).eq('Trevor').to_update.should be_a Hash
-      end
-
     end
   end
 end
